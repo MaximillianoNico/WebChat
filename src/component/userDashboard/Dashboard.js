49aa56f2aab59f10d291b053ca01cbb18948b27d
Maximilliano <maximillianonico8@gmail.com>
2018-04-24 15:24:27 +0700

    Editing Template Dashboard

--- src/component/userDashboard/Dashboard.js
+++ src/component/userDashboard/Dashboard.js
@@ -1,6 +1,7 @@
 import React,{Component} from 'react';
 import {Redirect} from 'react-router-dom';
 import {connect} from 'react-redux';
+import './src/style.scss';
 class Dashboard extends Component{
     render(){
         if(this.props.user!= null){
@@ -9,7 +10,7 @@ class Dashboard extends Component{
                     <nav className="navbar navbar-dark fixed-top bg-light flex-md-nowrap p-0 shadow">
                         <div className="navbar-brand col-3 mr-0 bg-dark">
                             <div style={{float:'left'}}>
-                                <i class="fas fa-user-circle" style={{fontSize:40,padding:10}}></i>
+                                {/* <i class="fas fa-user-circle" style={{fontSize:40,padding:10}}></i> */}
                             </div>
                             <div className="" style={{float:'right'}}>
                                 <i class="far fa-comments" style={{fontSize:25,paddingTop:20,paddingBottom:20,paddingLeft:10,paddingRight:10}}></i>
@@ -17,7 +18,15 @@ class Dashboard extends Component{
                             </div>
                         </div>
                         <div className="navbar-nav w-100" style={{paddingLeft:20}}>
-                            <h3>Title</h3>
+                            <div className="row">
+                                <div className="col-1">
+                                    <i class="fas fa-user-circle" style={{fontSize:60,paddingTop:5}}></i>
+                                </div>
+                                <div className="col-11" style={{paddingTop:5}}>
+                                    <h2>Jhon Doe</h2>
+                                    <p style={{margin:0}}>lorem</p>
+                                </div>
+                            </div>
                         </div>
                     </nav>
                     <div className="container-fluid" style={{marginTop:69}}>
@@ -25,44 +34,101 @@ class Dashboard extends Component{
                             <nav className="col-3 d-none d-md-block bg-light sidebar scrollbar" style={{overflowY:'scroll',height:'90vh'}}>
                                 <div className="container-fluid sidebar-sticky">
                                     {/* <h1>Menu</h1> */}
+                                    <nav class="navbar navbar-light bg-light" style={{margin:0,paddingTop:20}}>
+                                            <form class="form-inline row col-12" style={{margin:0,padding:0}}>
+                                                <input class="form-control col-10" type="search" placeholder="Search" aria-label="Search"/>
+                                                <button class="btn btn-xs btn-outline-success col-2 my-2 my-sm-0" type="submit">
+                                                    <i class="fas fa-search"></i>
+                                                </button>
+                                            </form>
+                                        </nav>
                                     <div class="sidebar-sticky">
+                                        
                                         <ul class="nav flex-column">
-                                        <li class="nav-item">
-                                            <a class="nav-link active" href="#">
-                                            <svg xmlns="http://www.w3.org/2000/svg" class="feather feather-home" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" width="24" height="24"><path d="M 3 9 l 9 -7 l 9 7 v 11 a 2 2 0 0 1 -2 2 H 5 a 2 2 0 0 1 -2 -2 Z" /><polyline points="9,22 9,12 15,12 15,22" /></svg>
-                                            Dashboard <span class="sr-only">(current)</span>
-                                            </a>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a class="nav-link" href="#">
-                                            <svg xmlns="http://www.w3.org/2000/svg" class="feather feather-file" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" width="24" height="24"><path d="M 13 2 H 6 a 2 2 0 0 0 -2 2 v 16 a 2 2 0 0 0 2 2 h 12 a 2 2 0 0 0 2 -2 V 9 Z" /><polyline points="13,2 13,9 20,9" /></svg>
-                                            Orders
-                                            </a>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a class="nav-link" href="#">
-                                            <svg xmlns="http://www.w3.org/2000/svg" class="feather feather-shopping-cart" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" width="24" height="24"><circle cx="9" cy="21" r="1" /><circle cx="20" cy="21" r="1" /><path d="M 1 1 h 4 l 2.68 13.39 a 2 2 0 0 0 2 1.61 h 9.72 a 2 2 0 0 0 2 -1.61 L 23 6 H 6" /></svg>
-                                            Products
-                                            </a>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a class="nav-link" href="#">
-                                            <svg xmlns="http://www.w3.org/2000/svg" class="feather feather-users" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" width="24" height="24"><path d="M 17 21 v -2 a 4 4 0 0 0 -4 -4 H 5 a 4 4 0 0 0 -4 4 v 2" /><circle cx="9" cy="7" r="4" /><path d="M 23 21 v -2 a 4 4 0 0 0 -3 -3.87" /><path d="M 16 3.13 a 4 4 0 0 1 0 7.75" /></svg>
-                                            Customers
-                                            </a>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a class="nav-link" href="#">
-                                            <svg xmlns="http://www.w3.org/2000/svg" class="feather feather-bar-chart-2" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" width="24" height="24"><line x1="18" y1="20" x2="18" y2="10" /><line x1="12" y1="20" x2="12" y2="4" /><line x1="6" y1="20" x2="6" y2="14" /></svg>
-                                            Reports
-                                            </a>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a class="nav-link" href="#">
-                                            <svg xmlns="http://www.w3.org/2000/svg" class="feather feather-layers" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" width="24" height="24"><polygon points="12,2 2,7 12,12 22,7 12,2" /><polyline points="2,17 12,22 22,17" /><polyline points="2,12 12,17 22,12" /></svg>
-                                            Integrations
-                                            </a>
-                                        </li>
+                                            <li class="nav-item">
+                                                <a class="nav-link active " href="#">
+                                                    <div className="row" style={{paddingTop:15,paddingBottom:0}}>
+                                                        <i class="fas fa-user-circle" style={{fontSize:50,padding:3}}></i>
+                                                        <div className="col-8">
+                                                            <h5>Jhon Doe</h5> <span class="sr-only">(current)</span>
+                                                            <p>lorem</p>
+                                                        </div>
+                                                    </div>
+                                                </a>
+                                                <hr style={{margin:0}}/>
+                                            </li>
+                                            <li class="nav-item">
+                                                <a class="nav-link active " href="#">
+                                                    <div className="row" style={{paddingTop:15,paddingBottom:0}}>
+                                                        <i class="fas fa-user-circle" style={{fontSize:50,padding:3}}></i>
+                                                        <div className="col-8">
+                                                            <h5>Rachel</h5> <span class="sr-only">(current)</span>
+                                                            <p>lorem</p>
+                                                        </div>
+                                                    </div>
+                                                </a>
+                                                <hr style={{margin:0}}/>
+                                            </li>
+                                            <li class="nav-item">
+                                                <a class="nav-link active " href="#">
+                                                    <div className="row" style={{paddingTop:15,paddingBottom:0}}>
+                                                        <i class="fas fa-user-circle" style={{fontSize:50,padding:3}}></i>
+                                                        <div className="col-8">
+                                                            <h5>Angela</h5> <span class="sr-only">(current)</span>
+                                                            <p>lorem</p>
+                                                        </div>
+                                                    </div>
+                                                </a>
+                                                <hr style={{margin:0}}/>
+                                            </li>
+                                            <li class="nav-item">
+                                                <a class="nav-link active " href="#">
+                                                    <div className="row" style={{paddingTop:15,paddingBottom:0}}>
+                                                        <i class="fas fa-user-circle" style={{fontSize:50,padding:3}}></i>
+                                                        <div className="col-8">
+                                                            <h5>Andre</h5> <span class="sr-only">(current)</span>
+                                                            <p>lorem</p>
+                                                        </div>
+                                                    </div>
+                                                </a>
+                                                <hr style={{margin:0}}/>
+                                            </li>
+                                            <li class="nav-item">
+                                                <a class="nav-link active " href="#">
+                                                    <div className="row" style={{paddingTop:15,paddingBottom:0}}>
+                                                        <i class="fas fa-user-circle" style={{fontSize:50,padding:3}}></i>
+                                                        <div className="col-8">
+                                                            <h5>Steve</h5> <span class="sr-only">(current)</span>
+                                                            <p>lorem</p>
+                                                        </div>
+                                                    </div>
+                                                </a>
+                                                <hr style={{margin:0}}/>
+                                            </li>
+                                            <li class="nav-item">
+                                                <a class="nav-link active " href="#">
+                                                    <div className="row" style={{paddingTop:15,paddingBottom:0}}>
+                                                        <i class="fas fa-user-circle" style={{fontSize:50,padding:3}}></i>
+                                                        <div className="col-8">
+                                                            <h5>Emilia</h5> <span class="sr-only">(current)</span>
+                                                            <p>lorem</p>
+                                                        </div>
+                                                    </div>
+                                                </a>
+                                                <hr style={{margin:0}}/>
+                                            </li>
+                                            <li class="nav-item">
+                                                <a class="nav-link active " href="#">
+                                                    <div className="row" style={{paddingTop:15,paddingBottom:0}}>
+                                                        <i class="fas fa-user-circle" style={{fontSize:50,padding:3}}></i>
+                                                        <div className="col-8">
+                                                            <h5>Angela</h5> <span class="sr-only">(current)</span>
+                                                            <p>lorem</p>
+                                                        </div>
+                                                    </div>
+                                                </a>
+                                                <hr style={{margin:0}}/>
+                                            </li>
                                         </ul>
 
                                         <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
@@ -134,8 +200,53 @@ class Dashboard extends Component{
                                     </div>
                                 </div>
                             </nav>
-                            <main className="col-9 col-md-9 ml-sm-auto col-lg-9 px-4" style={{padding:10}} role="main">
-                                <h1>Content</h1>
+                            <main className="col-9 col-md-9 ml-sm-auto col-lg-9 px-4" style={{padding:10,overflowY:'scroll',overflowX:'hidden',height:'90vh'}} role="main">
+                                <div className="container-fluid" style={{marginTop:100,padding:0}}>
+                                
+                                    <div className="container">
+                                        <div className="row">
+                                            <div className="col-12 row">
+                                                <i class="fas fa-user-circle" style={{fontSize:30,paddingTop:5,paddingRight:15}}></i>
+                                                <div className="col-6 jumbotron" style={{padding:15}}>
+                                                    <p>Hello, How are you?</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="container">
+                                        <div className="row">
+                                            <div className="col-12 row">
+                                                {/* <i class="fas fa-user-circle" style={{fontSize:30,paddingTop:5,paddingRight:15}}></i> */}
+                                                <div className="col-6 jumbotron" style={{padding:15,marginRight:0,left:'50%'}}>
+                                                    <p>I'm fine. I heard you've become a developer, maybe we could work creating a startup?</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="container">
+                                        <div className="row">
+                                            <div className="col-12 row">
+                                                <i class="fas fa-user-circle" style={{fontSize:30,paddingTop:5,paddingRight:15}}></i>
+                                                <div className="col-6 jumbotron" style={{padding:15}}>
+                                                    <p></p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="container" style={{position:"fixed",bottom:10}}>
+                                        <center className="row">
+                                            <center className="col-12 row">
+                                                <button className="btn btn-lg btn-cycle btn-success" style={{textAlign:'center'}}>
+                                                    <i class="fas fa-plus" style={{fontSize:24,marginLeft:-7}}></i>
+                                                </button>
+                                                <input className="col-10 form-control form-chat" placeholder="Chatting..."></input>
+                                                <button className="btn btn-lg btn-cycle btn-info" style={{textAlign:'center'}}>
+                                                    <i class="fab fa-telegram-plane" style={{fontSize:24,marginLeft:-7}}></i>
+                                                </button>
+                                            </center>
+                                        </center>
+                                    </div>
+                                </div>
                             </main>
                         </div>
                     </div>
